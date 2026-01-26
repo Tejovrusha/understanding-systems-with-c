9d498b869fd7b179c771a6a75b1068bf1a380eb5
Tejovrusha <tejovrusha416@gmail.com>
2026-01-26 11:32:57 +0530

    Update decimal_to_binary.c

--- decimal_to_binary.c
+++ decimal_to_binary.c
@@ -2,15 +2,16 @@
 
 #include<stdio.h>
 
-int dec_to_bin(int x){
-  if(x==1) return 1;
-  return dec_to_bin(x/2)*10 + x%2;
+void dec_to_bin(long long int x){
+  if(x>1) dec_to_bin(x/2);
+  printf("%lld",x%2);
 }
 
 void main(){
-  int n;
+  long long int n;
   printf("Enter a decimal number: ");
-  scanf("%d",&n);
-  int y= dec_to_bin(n);
-  printf("The decimal number %d converted to Binary is %d",n,y);
+  scanf("%lld",&n);
+  printf("The decimal number %lld converted to Binary is ",n);
+  dec_to_bin(n);
+  printf("\n");
 }
